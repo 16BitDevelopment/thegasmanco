139535762f6f2ce5f8cd3f95552f373a86cd2577
16BitDevelopment <16bitdevelopment@gmail.com>
2025-12-18 21:24:26 +1100

    New Brighton added to locations

--- src/gasman.js
+++ src/gasman.js
@@ -25,6 +25,7 @@ export function getGasman(location) {
         "Hayters Hill",
         "Mcleods Shoot",
         "Mooball",
+        "New Brighton",
         "Skinners Shoot",
         "South Golden Beach",
         "Suffolk Park",
